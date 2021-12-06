70f175121208c0409c46a942cab1e1713667f9c3
vrbr <lpvamaria@gmail.com>
2021-12-06 18:31:34 +0200

    Блок reviews на мобиле, подключен слайдер

---
+++ js/reviews-swiper.js
@@ -0,0 +1,30 @@
+const reviewsSwiper = new Swiper(".reviews-swiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    initialSlide: 2,
+    loop: true,
+    navigation: {
+        nextEl: ".reviews-swiper-button-next",
+        prevEl: ".reviews-swiper-button-prev",
+    },
+    pagination: {
+        el: ".reviews-swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+        dynamicMainBullets: 1,
+    },
+    // breakpoints: {
+    //     640: {
+    //         slidesPerView: 2,
+    //         spaceBetween: 20,
+    //     },
+    //     768: {
+    //         slidesPerView: 4,
+    //         spaceBetween: 40,
+    //     },
+    //     1024: {
+    //         slidesPerView: 5,
+    //         spaceBetween: 50,
+    //     },
+    // },
+});
