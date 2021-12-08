32de517dbf926af3e4c93377be2c829c3abebc33
vrbr <lpvamaria@gmail.com>
2021-12-08 15:50:51 +0200

    Завершен блок gallery на всех ресширениях. Отключен свайпер при 768

--- js/gallery-swiper.js
+++ js/gallery-swiper.js
@@ -1,23 +1,19 @@
-const slider = document.querySelector('.gallery-swiper');
-let gallerySwiper;
-
-const sliderInit = () => {
-    gallerySwiper = new Swiper(slider, {
-        autoHeight: true,
-        slidesPerView: "auto",
-        spaceBetween: 10,
-        navigation: {
-            nextEl: ".gallery-swiper-button-next",
-            prevEl: ".gallery-swiper-button-prev",
-        },
-    })
-}
-
 document.addEventListener('DOMContentLoaded', () => {
     const width = window.innerWidth;
-    if (width <= 768){
+    if (width < 768){
+        const slider = document.querySelector('.gallery-swiper');
+
+        const sliderInit = () => {
+            gallerySwiper = new Swiper(slider, {
+                autoHeight: true,
+                slidesPerView: "auto",
+                spaceBetween: 10,
+                navigation: {
+                    nextEl: ".gallery-swiper-button-next",
+                    prevEl: ".gallery-swiper-button-prev",
+                },
+            })
+        }
         sliderInit();
-    } else {
-        gallerySwiper.destroy();
     }
 });
