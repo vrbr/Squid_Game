5e420a7f0d63be068494c65686d1a7fd39f87127
vrbr <lpvamaria@gmail.com>
2021-12-08 14:50:05 +0200

    Блок gallery на планшете

--- js/gallery-swiper.js
+++ js/gallery-swiper.js
@@ -1,9 +1,23 @@
-const gallerySwipe = new Swiper(".gallery-swiper", {
-    autoHeight: true,
-    slidesPerView: "auto",
-    spaceBetween: 10,
-    navigation: {
-        nextEl: ".gallery-swiper-button-next",
-        prevEl: ".gallery-swiper-button-prev",
-    },
+const slider = document.querySelector('.gallery-swiper');
+let gallerySwiper;
+
+const sliderInit = () => {
+    gallerySwiper = new Swiper(slider, {
+        autoHeight: true,
+        slidesPerView: "auto",
+        spaceBetween: 10,
+        navigation: {
+            nextEl: ".gallery-swiper-button-next",
+            prevEl: ".gallery-swiper-button-prev",
+        },
+    })
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const width = window.innerWidth;
+    if (width <= 768){
+        sliderInit();
+    } else {
+        gallerySwiper.destroy();
+    }
 });
